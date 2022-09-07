579c17aa48ddc658ff068afb345129a1d7e5aa3a
DESKTOP-STN70LP\User <yarinbazi@gmail.com>
2022-09-07 11:43:23 +0300

    adding one way data binding

--- FacebookWinFormsApp/UserLikedPagesController.cs
+++ FacebookWinFormsApp/UserLikedPagesController.cs
@@ -17,53 +17,7 @@ namespace FacebookAppGUI
          
         public void FetchData()
         {
-            try
-            {
-                m_LabelError.Invoke(new Action(() => m_LabelError.Visible = false));
-                m_ListBoxLikePages.Invoke(new Action(() => m_ListBoxLikePages.Items.Clear()));
-                foreach (Page page in Facade.LikedPages)
-                {
-                    m_ListBoxLikePages.Invoke(new Action(() => m_ListBoxLikePages.Items.Add(page)));
-                }
-
-                if (m_ListBoxLikePages.Items.Count == 0)
-                {
-                    m_ListBoxLikePages.Invoke(new Action(() => m_ListBoxLikePages.Items.Add("You doesn't have like pages to show")));
-                }
-            }
-            catch (Exception exception)
-            {
-               // m_LabelError.Invoke(new Action(() => m_LabelError.Visible = true));
-            }
-        }
-
-        private void m_ListBoxLikePages_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            displaySelectedLikePage();
-        }
-
-        private void displaySelectedLikePage()
-        {
-            try
-            {
-                Page selectedLikePage;
-
-                m_LabelError.Visible = false;
-                if (m_ListBoxLikePages.SelectedItems.Count == 1)
-                {
-                    selectedLikePage = m_ListBoxLikePages.SelectedItem as Page;
-                    if (selectedLikePage != null)
-                    {
-                        m_PanelLikePages.Visible = true;
-                        m_LabelLikePageName.Text = selectedLikePage.Name;
-                        m_PictureBoxLikePagesPicture.LoadAsync(selectedLikePage.PictureNormalURL);
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                m_LabelError.Visible = true;
-            }
+            pageBindingSource.DataSource = Facade.LikedPages;
         }
     }
 }
